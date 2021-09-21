6a5f8c2a2c1486484624fa9612c95a63e545b6ae
Meseret <meseret.kassayes@gmail.com>
2021-09-21 19:19:08 +0300

    Push notification is added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-if(navigator.serviceWorker){
+if(navigator.serviceWorker && 'PushManager' in window){
     window.addEventListener('load',()=>{
-        navigator.serviceWorker
+     const sw=   navigator.serviceWorker
         .register('../sw_cached_sites.js')
         .then(response=>console.log('SW registered'))
         .catch(error=>console.error(`Something is happening with :${error}`))
+
+        //ask push notification permision
+        const permission = window.Notification.requestPermission();
+        if(Notification.permission==='granted'){
+            showNotification('Hello there','This SW push notification',sw)
+            console.log('granted')
+        }
     })
-    console.log('SW is supported')
+}
+
+const showNotification =(title,body,sw)=>{
+    //sw.showNotification(title,body)
+    console.log('send')
 }
